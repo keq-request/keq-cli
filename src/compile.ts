3f64462c0dc2ed2c845c812d6a32e32135e1836b
val.istar.guo <val.istar.guo@gmail.com>
2021-05-11 22:06:18 +0800

    feat: can get swagger file from url
    fix #4

--- src/compile.ts
+++ src/compile.ts
@@ -3,16 +3,33 @@ import * as yaml from 'js-yaml'
 import * as path from 'path'
 import { genModuleCode } from './gencode'
 import { Options } from './interface'
+import validUrl from 'valid-url'
+import { request } from 'keq'
 
 
 export async function compile(moduleName: string, filepath: string, options: Options): Promise<void> {
-  const fileext = path.extname(filepath)
-  const content = await fs.readFile(filepath, 'utf8')
-  if (['.yml', '.yaml'].includes(fileext)) {
-    await genModuleCode(moduleName, yaml.load(content), options)
-  } else if (fileext === '.json') {
-    await genModuleCode(moduleName, JSON.parse(content), options)
+  if (validUrl.isUri(filepath)) {
+    const res = await request
+      .get(filepath)
+      .option('resolveWithFullResponse')
+
+    let content = await res.text()
+    try {
+      content = JSON.parse(content)
+    } catch (e) {
+      throw new Error(`The swagger file get from url isn't json: ${filepath}`)
+    }
+
+    await genModuleCode(moduleName, content, options)
   } else {
-    throw new Error(`File ${fileext} not support.`)
+    const fileext = path.extname(filepath)
+    const content = await fs.readFile(filepath, 'utf8')
+    if (['.yml', '.yaml'].includes(fileext)) {
+      await genModuleCode(moduleName, yaml.load(content), options)
+    } else if (fileext === '.json') {
+      await genModuleCode(moduleName, JSON.parse(content), options)
+    } else {
+      throw new Error(`File ${fileext} not support.`)
+    }
   }
 }
