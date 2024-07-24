8b6e580f2fda5828205b8b87e8b44c1cb1b6968a
val.istar.guo <val.istar.guo@gmail.com>
2024-07-24 17:30:00 +0800

    build: optimizing the build script

--- build/watch.sh
+++ build/watch.sh
@@ -3,5 +3,5 @@ NODE_ENV=development
 
 cpx "./src/**/*.{hbs,json}" "dist/umd/src" --watch &
 cpx "./src/**/*.{hbs,json}" "dist/esm/src" --watch &
-tsc -p build/tsconfig.lib.json -w &
-tsc -p build/tsconfig.es.json -w
+tsc -p build/tsconfig.umd.json -w &
+tsc -p build/tsconfig.esm.json -w
