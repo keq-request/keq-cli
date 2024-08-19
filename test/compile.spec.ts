e30b2eacaf64bec2dbe4907818e70fa6df7f6c5f
val.istar.guo <val.istar.guo@gmail.com>
2024-08-19 14:23:56 +0800

    test: disinfect swagger

--- test/compile.spec.ts
+++ test/compile.spec.ts
@@ -3,12 +3,13 @@ import { OpenAPIV3 } from 'openapi-types'
 import { compile } from '~/compile-openapi'
 import * as swagger from './swagger.json'
 import { FileNamingStyle } from '~/types/file-naming-style'
+import { disinfect } from '~/utils/disinfect'
 
 
 test('compile swagger', async () => {
   const files = await compile({
     moduleName: 'test',
-    document: swagger as unknown as OpenAPIV3.Document,
+    document: await disinfect('test', swagger as unknown as OpenAPIV3.Document),
     outdir: './outdir',
     fileNamingStyle: FileNamingStyle.snakeCase,
     strict: false,
