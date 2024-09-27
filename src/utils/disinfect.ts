91089080e6379dd43fbc150483165857eb0c10a0
val.istar.guo <val.istar.guo@gmail.com>
2024-09-27 14:21:36 +0800

    fix: avoid Chinese characters being replaced by random numbers

--- src/utils/disinfect.ts
+++ src/utils/disinfect.ts
@@ -5,8 +5,13 @@ import { fixSwagger } from 'swagger-fix'
 import { validateSwagger3 } from './validate-swagger3'
 
 export async function disinfect(moduleName: string, swagger: OpenAPI.Document): Promise<OpenAPIV3.Document> {
-  let swagger3 = await toSwagger3(swagger)
-  swagger3 = fixSwagger(swagger3)
+  const swagger2 = fixSwagger(swagger)
+  /**
+   * NOTE: fixSwagger should before toSwagger3
+   *       because toSwagger3 will replace Chinese with random numbers
+   *       which will reduce readability
+   */
+  const swagger3 = await toSwagger3(swagger2)
 
   removeUnnecessaryRef(swagger3)
 
